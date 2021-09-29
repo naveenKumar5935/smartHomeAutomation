f5ca39cce6d025ea0ddb7ea280879e2c010fa943
naveenkumar <naveenbti002@gmail.com>
2021-09-29 01:58:18 -0400

    created layout for login signup pages and added appropriate activities for them

---
+++ smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/RegisterActivity.java
@@ -0,0 +1,15 @@
+package ca.theautomators.it.smarthomeautomation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+
+    }
+}
