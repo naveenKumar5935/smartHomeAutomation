b9e891ea8624fa77a034b5585e7b6a7d29fa6d60
naveenkumar <naveenbti002@gmail.com>
2021-09-29 02:50:11 -0400

    added example code for  RegisterActivity

--- smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/RegisterActivity.java
+++ smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/RegisterActivity.java
@@ -2,13 +2,48 @@ package ca.theautomators.it.smarthomeautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import ca.theautomators.it.smarthomeautomation.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
+    String userEmail, userPassword, accessCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+        binding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userEmail = binding.registerEmailTb.getText().toString();
+                userPassword = binding.registerPasswordTb.getText().toString();
+                accessCode = binding.registerAccessTb.getText().toString();
+                if(!accessCode.matches("123456")){
+                    Toast.makeText(getApplicationContext(),"access code invalid",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Signup success",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
+
     }
 }
