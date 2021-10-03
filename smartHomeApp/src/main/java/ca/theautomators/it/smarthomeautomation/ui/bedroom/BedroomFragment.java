e1f3c33520bde1df59c4bb05e1eb2ae743f6f5cc
Dood <riccis2@gmail.com>
2021-10-03 17:27:50 -0400

    Bedroom fragment layout Added updatable text (dummy values for now) Added switch for light control Added slider for light hue control

--- smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/ui/bedroom/BedroomFragment.java
+++ smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/ui/bedroom/BedroomFragment.java
@@ -15,15 +15,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 import ca.theautomators.it.smarthomeautomation.R;
 
 public class BedroomFragment extends Fragment {
 
+    private String bedroomData;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_bedroom, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        bedroomData = "Temp: 21 degrees C\nHumidity: 30%\nLights: ON";
+
+        TextView dataText = (TextView) root.findViewById(R.id.bedroom_data);
+        dataText.setText(bedroomData);
         return root;
     }
 }
