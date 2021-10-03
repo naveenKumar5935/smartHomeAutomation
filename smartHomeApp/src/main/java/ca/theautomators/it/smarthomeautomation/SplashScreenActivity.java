d26eef5fd484f7033701a09668d136775b998c54
gaganajeetHanny0705 <n01350705@humbermail.ca>
2021-10-03 08:23:23 -0400

    Added Splash Screen

---
+++ smartHomeApp/src/main/java/ca/theautomators/it/smarthomeautomation/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package ca.theautomators.it.smarthomeautomation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreenActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
